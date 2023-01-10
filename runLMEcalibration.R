0ebd2a7ea3a2640af1d9c1e65ea245b3759db4ed
juliablanchard <julia.blanchard@utas.edu.au>
2023-01-11 09:35:58 +1100

    comment about pbsapply

--- runLMEcalibration.R
+++ runLMEcalibration.R
@@ -4,20 +4,20 @@
 # for (i in 1:lmenum) LMEs[i,c(2:6)]<-LHSsearch(LMEs[i,1],iter=100)
 # saveRDS(LMEs,"bestvals_LMEs.RDS")
 
-# faster using pbsapply, in the LHSsearch pbapply has cl=6 which uses cluster to run in parallel, but here it is run sequentially.
+# faster using pbsapply, in the LHSsearch pbapply has cl=6 which uses cluster to run in parallel, but here it is run sequentially if cl is not specified.
 lmenum=66
-lmes<-t(pbsapply(X=1:lmenum,LHSsearch,iter=100))
-saveRDS(lmes,"bestvals_LMEs_round2.RDS")
+lmes<-t(pbsapply(X=1:lmenum,LHSsearch,iter=300,cl=2))
+saveRDS(lmes,"bestvals_LMEs_iter300.RDS")
 
 ############### Make plots
 
 #### Check other model performance indicators using the above estimates
-bestvals<-data.frame(readRDS("bestvals_LMEs.RDS"))
+bestvals<-data.frame(readRDS("bestvals_LMEs.RDS_iter300"))
 
 # add column for correlation:
 bestvals$cor<-rep(0,lmenum)
 
-pdf("CalibrationPlots.pdf",height = 6, width = 8)
+pdf("CalibrationPlots_iter100.pdf",height = 6, width = 8)
 
 for (i in 1:66){
   
@@ -78,48 +78,6 @@ saveRDS(bestvals,"bestvals_LMEs_cor.RDS")
 
 # library(optimParallel)
 #  set up workers
-noCores <- parallel::detectCores() - 1 # keep some spare core
-cl <- parallel::makeCluster(noCores, setup_timeout = 0.5)
-setDefaultCluster(cl = cl)
-clusterExport(cl, varlist = "cl",envir=environment())
-clusterEvalQ(cl, {
-  library(optimParallel)
-  source("LME_calibration.R")
-})
-
-LMEnum=22
-lme_input<-get_lme_inputs(LMEnumber=LMEnum)
-optim_out<- optimParallel::optimParallel(par=as.numeric(bestvals[LMEnum,c(2:5)]),getError, input=lme_input, LMEnumber=LMEnum)
-stopCluster(cl)
-
-bestvals[LMEnum,c(1:4)]<-optim_out$par
-
-bestvals[LMEnum,5]<-optim_out$value
-
-saveRDS(bestvals,"optim_vals_LME.RDS")
-
-
-########## to save all error estimates:
-
-# # set up parameter set to run initial simulations
-# set.seed(1234)
-# 
-# # num "individual runs"
-# num_iter=100
-# simset <- data.frame(randomLHS(num_iter, 4))
-# # rows are iterations, columns are specific parameters
-# colnames(simset )<-c("f.u","f.v","f.minu","f.minv")
-# # adjust range of mi size params, others go form 0-1
-# simset [,"f.minu"]<-simset [,"f.minu"]*2
-# simset [,"f.minv"]<-simset [,"f.minv"]*2
-# 
-# LMEnumber=c(1:66)
-# 
-# simset<-expand_grid(LMEnumber,simset)
-# 
-# simset$rmse<-0
-# 
-# #  set up workers for parallel runs
 # noCores <- parallel::detectCores() - 1 # keep some spare core
 # cl <- parallel::makeCluster(noCores, setup_timeout = 0.5)
 # setDefaultCluster(cl = cl)
@@ -129,11 +87,53 @@ saveRDS(bestvals,"optim_vals_LME.RDS")
 #   source("LME_calibration.R")
 # })
 # 
-# # run model and output error term for each row (in parallel)
-# simset$rmse<-pbapply(X=simset[,c(1:5)],1,getError,cl=cl)
-# 
+# LMEnum=22
+# lme_input<-get_lme_inputs(LMEnumber=LMEnum)
+# optim_out<- optimParallel::optimParallel(par=as.numeric(bestvals[LMEnum,c(2:5)]),getError, input=lme_input, LMEnumber=LMEnum)
 # stopCluster(cl)
 # 
-# #save the file
-# saveRDS(simset,"simset_LMEs.RDS")
+# bestvals[LMEnum,c(1:4)]<-optim_out$par
+# 
+# bestvals[LMEnum,5]<-optim_out$value
+# 
+# saveRDS(bestvals,"optim_vals_LME.RDS")
+# 
+# 
+# ########## to save all error estimates:
+# 
+# # # set up parameter set to run initial simulations
+# # set.seed(1234)
+# # 
+# # # num "individual runs"
+# # num_iter=100
+# # simset <- data.frame(randomLHS(num_iter, 4))
+# # # rows are iterations, columns are specific parameters
+# # colnames(simset )<-c("f.u","f.v","f.minu","f.minv")
+# # # adjust range of mi size params, others go form 0-1
+# # simset [,"f.minu"]<-simset [,"f.minu"]*2
+# # simset [,"f.minv"]<-simset [,"f.minv"]*2
+# # 
+# # LMEnumber=c(1:66)
+# # 
+# # simset<-expand_grid(LMEnumber,simset)
+# # 
+# # simset$rmse<-0
+# # 
+# # #  set up workers for parallel runs
+# # noCores <- parallel::detectCores() - 1 # keep some spare core
+# # cl <- parallel::makeCluster(noCores, setup_timeout = 0.5)
+# # setDefaultCluster(cl = cl)
+# # clusterExport(cl, varlist = "cl",envir=environment())
+# # clusterEvalQ(cl, {
+# #   library(optimParallel)
+# #   source("LME_calibration.R")
+# # })
+# # 
+# # # run model and output error term for each row (in parallel)
+# # simset$rmse<-pbapply(X=simset[,c(1:5)],1,getError,cl=cl)
+# # 
+# # stopCluster(cl)
+# # 
+# # #save the file
+# # saveRDS(simset,"simset_LMEs.RDS")
 # 
