be0ceda9620dcdae96119c37b0f1a0cb9ce91640
lidefi87 <lidefi87@hotmail.com>
2024-10-16 11:54:01 +1100

    removing new functions from scripts'

--- new_workflow/processing_monthly_dbpm_inputs.R
+++ new_workflow/processing_monthly_dbpm_inputs.R
@@ -1,65 +1,27 @@
-# STEP 1: GET GCM INPUTS FOR DYNAMIC BENTHIC-PELAGIC SIZE SPECTRUM MODEL
+# Processing monthly inputs for Dynamic Benthic-Pelagic Size Spectrum Model 
+# (DBPM)
 
 # Loading libraries -------------------------------------------------------
-library(tidyverse)
+library(stringr)
 library(data.table)
-library(parallel)
-library(dtplyr)
+library(dplyr)
 library(arrow)
-library(janitor)
-source("supporting_functions.R")
-source("dbpm_model_functions.R")
+library(ggplot2)
+source("new_workflow/useful_functions.R")
 
-
-
-# Define functions --------------------------------------------------------
-dbpm_input_calc <- function(file_path, path_out){
-  # Inputs:
-  # - file_path (character) File path where monthly data is located
-  # - path_out (character) File path where outputs should be stored as parquet
-  # files
-  
-  #Outputs:
-  # - None. This function saves results as parquet files in the path provided.
-  
-  df <- read_parquet(file_path) |> 
-    clean_names() |> 
-    rename(sphy = phypico_vint_mol_m_2) |> 
-    mutate(lphy = phyc_vint_mol_m_2 - sphy) |> 
-    select(!phyc_vint_mol_m_2) |> 
-    rename(t = time, depth = depth_m, sbt = tob_deg_c, sst = tos_deg_c, 
-           expcbot = expc_bot_mol_m_2_s_1, area_m2 = tot_area_m2) |> 
-    mutate(er = getExportRatio(sphy, lphy, sst, depth),
-           er = ifelse(er < 0, 0, ifelse(er > 1, 1, er)),
-           intercept = GetPPIntSlope(sphy, lphy, mmin = 10^-14.25, 
-                                     mmid = 10^-10.184, mmax = 10^-5.25, depth, 
-                                     output = "intercept"),
-           slope = GetPPIntSlope(sphy, lphy, mmin = 10^-14.25, mmid = 10^-10.184, 
-                                 mmax = 10^-5.25, depth, output = "slope")) |> 
-    relocate(all_of(c("er", "intercept", "slope")), .before = sphy)
-  
-  #Save results
-  df |> 
-    write_parquet(path_out)
-}
-
-
-# LMEs --------------------------------------------------------------------
-#Calculating additional inputs from GFDL data
+# Calculating additional inputs from GFDL data ----------------------------
 monthly_inputs <- file.path("/g/data/vf71/la6889/dbpm_inputs", 
                             "monthly_weighted_mean") |> 
-  list.files(full.names = T)
+  list.files("raw-inputs", full.names = T)
 
 for(f in monthly_inputs){
-  f_out  <- str_replace(f, "all-inputs", "climate-inputs-slope")
+  f_out  <- str_replace(f, "raw-inputs", "climate-inputs-slope")
   dbpm_input_calc(f, f_out)
 }
 
-
 #Getting information about depth and area. We can choose any experiment within
-#the region of interest
-depth_area <- read_parquet(monthly_inputs[1], 
-                           col_select = c("depth_m", "tot_area_m2")) |> 
+#the region of interest.
+depth_area <- read_parquet(f, col_select = c("depth_m", "tot_area_m2")) |> 
   distinct()
 
 ## Loading effort and catches data ----------------------------------------
@@ -69,14 +31,15 @@ effort_file_path <- "/g/data/vf71/fishmip_inputs/ISIMIP3a/DKRZ_EffortFiles"
 effort_LME <- file.path(effort_file_path,
                         "effort_isimip3a_histsoc_1841_2010.csv") |> 
   fread() |> 
-  #Removing LME = 0 because there is no information about area
+  #Selecting LME of interest
   filter(LME == 61) |> 
   # calculate sum of effort by LME/by total area of LME 
   group_by(Year, LME) |> 
-  summarize(total_nom_active = sum(NomActive, na.rm = T), 
+  summarise(total_nom_active = sum(NomActive, na.rm = T), 
             .groups = "drop") |> 
   ungroup() |>
   rename(year = Year, region = LME) |> 
+  # Adding depth and area information for the LME of interest
   mutate(depth = depth_area$depth_m, 
          area_m2 = depth_area$tot_area_m2,
          total_nom_active_area_m2 = total_nom_active/area_m2)
@@ -85,15 +48,16 @@ effort_LME <- file.path(effort_file_path,
 LME_catch_input <- file.path(effort_file_path,
                              "catch-validation_isimip3a_histsoc_1850_2004.csv") |> 
   fread() |>
-  #Removing LME = 0 because there is no information about area
+  #Selecting LME of interest
   filter(LME == 61) |> 
-  # catch is in tonnes, checked in FishingEffort Rproject
+  # catch is in tonnes. This was checked in "FishingEffort" project
   mutate(catch_tonnes = Reported+IUU) |> 
   group_by(Year, LME) |> 
-  summarize(catch_tonnes = sum(catch_tonnes), .groups = "drop") |> 
+  summarise(catch_tonnes = sum(catch_tonnes), .groups = "drop") |> 
   # also Reg advise to exclude discards 
   ungroup() |> 
   rename(year = Year, region = LME) |> 
+  # Adding depth and area information for the LME of interest
   mutate(depth = depth_area$depth_m, 
          area_m2 = depth_area$tot_area_m2,
          catch_tonnes_area_m2 = catch_tonnes/area_m2)
@@ -133,11 +97,8 @@ DBPM_LME_effort_catch_input |>
 #Saving result that matches previous work
 ggsave("Output/Effort_LME_61_check_DFA.pdf", device = "pdf")
 
-#Removing variables not in use
-rm(plot_df)
 
 ## Saving catch and effort, and inputs data -------------------------------
-#Saving catch and effort data 
 DBPM_LME_effort_catch_input |> 
   write_parquet(file.path("/g/data/vf71/la6889/dbpm_inputs/",
                           "DBPM_LME_61_effort_catch_input.parquet"))
