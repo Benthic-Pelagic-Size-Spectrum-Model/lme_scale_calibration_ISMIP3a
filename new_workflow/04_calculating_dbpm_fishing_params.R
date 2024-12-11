a3521170cba87a6b911653e4db04301ca36ba294
lidefi87 <lidefi87@hotmail.com>
2024-12-11 16:17:39 +1100

    cleaning up script calculating fishing parameters

--- new_workflow/04_calculating_dbpm_fishing_params.R
+++ new_workflow/04_calculating_dbpm_fishing_params.R
@@ -1,111 +1,120 @@
 
 # Loading libraries -------------------------------------------------------
+source("new_workflow/useful_functions.R")
 library(dplyr)
 library(arrow)
-library(lhs)
 library(jsonlite)
-library(lubridate)
-source("new_workflow/useful_functions.R")
+library(purrr)
 
 
 # Loading DBPM climate and fishing inputs ---------------------------------
+region_int <- "fao-58"
 dbpm_inputs <- file.path("/g/data/vf71/la6889/dbpm_inputs/east_antarctica", 
                          "monthly_weighted", 
-                         "dbpm_clim-fish-inputs_fao-58_1841-2010.parquet") |> 
+                         paste0("dbpm_clim-fish-inputs_", region_int, 
+                                "_1841-2010.parquet")) |> 
   read_parquet()
 
 
 # Searching best fishing parameters values for area of interest -----------
+#Path to folder where results will be stored
+results_folder <- paste0("new_workflow/outputs/best_fish_vals_", region_int)
+#Number of iterations
 no_iter <- 100
 params_calibration <- LHSsearch(num_iter = no_iter, forcing_file = dbpm_inputs, 
                                 gridded_forcing = NULL, 
-                                best_val_folder = 
-                                  file.path("new_workflow/outputs",
-                                            "best_fish_vals"))
+                                best_val_folder = results_folder, 
+                                best_param = F)
 
 ## Creating plots with fishing parameters calculated above --------------
-#Path to folder where figures will be stored
-figure_folder <- "new_workflow/outputs/best_fish_vals"
-
 # Calculate errors and correlations with tuned fishing parameters and save plot
-params_correlation  <- corr_calib_plots(params_calibration, dbpm_inputs, 
-                                        figure_folder)
+params_corr <- params_calibration |> 
+  split(params_calibration$rmse) |>
+  map_df(\(x) getError(x, dbpm_inputs, corr = T))
 
 #Adding correlation to fishing parameter data frame
 params_calibration <- params_calibration |> 
-  left_join(params_correlation, by = join_by(region)) |> 
+  select(!region) |> 
+  bind_cols(params_corr) |> 
+  filter(cor >= 0) |> 
+  arrange(desc(cor), rmse) |> 
   relocate(region, .before = fmort_u)
 
+#Saving results
 params_calibration |> 
-  write_parquet(file.path(figure_folder, 
-                          "best-fishing-parameters_FAO-58_searchvol_estimated_numb-iter_100.parquet"))
+  write_parquet(file.path(
+    results_folder,
+    paste0("best-fishing-parameters_", region_int, 
+           "_searchvol_estimated_numb-iter_100.parquet")))
+
+params_calibration |> 
+  slice(1) |> 
+  corr_calib_plots(dbpm_inputs, results_folder)
+
+params_calibration |> 
+  arrange(rmse) |>
+  slice(1) |> 
+  corr_calib_plots(dbpm_inputs, results_folder)
+
 
 ## Optimising underperforming regions --------------------------------------
 # Since correlation is below 0.5 and the plots comparing estimates and obs do 
 # not look like a great fit, we will calculate fishing parameters again 
 no_iter <- 1000
-params_calibration <- LHSsearch(num_iter = no_iter, seed = 42,
-                                forcing_file = dbpm_inputs, 
-                                gridded_forcing = NULL, 
-                                best_val_folder = 
-                                  file.path("new_workflow/outputs",
-                                            "best_fish_vals"))
+params_calibration_optim <- LHSsearch(num_iter = no_iter, seed = 123,
+                                      forcing_file = dbpm_inputs, 
+                                      gridded_forcing = NULL, 
+                                      best_val_folder = results_folder, 
+                                      best_param = F)
 
+params_corr <- params_calibration_optim |> 
+  split(params_calibration_optim$rmse) |>
+  map_df(\(x) getError(x, dbpm_inputs, corr = T))
 
+#Adding correlation to fishing parameter data frame
+params_calibration_optim <- params_calibration_optim |> 
+  select(!region) |> 
+  bind_cols(params_corr) |> 
+  filter(cor >= 0) |> 
+  arrange(desc(cor), rmse) |> 
+  relocate(region, .before = fmort_u)
 
+#Saving results
+params_calibration_optim |> 
+  write_parquet(file.path(
+    results_folder,
+    paste0("best-fishing-parameters_", region_int, 
+           "_searchvol_estimated_numb-iter_", no_iter, ".parquet")))
 
+params_calibration_optim |> 
+  slice(1) |> 
+  corr_calib_plots(dbpm_inputs, results_folder)
 
+params_calibration_optim |> 
+  arrange(rmse) |> 
+  slice(1) |> 
+  corr_calib_plots(dbpm_inputs, results_folder)
 
 
 # Getting DBPM parameters -------------------------------------------------
+fishing_params <- params_calibration_optim |> 
+  arrange(rmse) |> 
+  slice(1)
+
 params <- sizeparam(dbpm_inputs, fishing_params, xmin_consumer_u = -3, 
                     xmin_consumer_v = -3, tstepspryr = 12)
 
 # Saving parameters
 params |> 
   #Ensuring up to 10 decimal places are saved in file
-  write_json("new_workflow/outputs/dbpm_size_params.json", digits = 10)
+  write_json(paste0("new_workflow/outputs/dbpm_size_params_", region_int, 
+                    ".json"), digits = 10)
 
 
 # Loading DBPM parameters -------------------------------------------------
 # If paramaters were already saved, they can be read instead of being 
 # recalculated
-params <- read_json("new_workflow/outputs/dbpm_size_params.json", 
+params <- read_json(paste0("new_workflow/outputs/dbpm_size_params_",
+                           region_int, ".json"), 
                     simplifyVector = T)
-
-
-# result_set <- sizemodel(params)
-
-
-
-# attach(params)
-
-
-
-
-
-
-
-
-
-
-
-gridded_params <- sizeparam(,
-                            
-                            equilibrium = FALSE, dx = 0.1, 
-                            xmin.consumer.u = -3, xmin.consumer.v = -3,
-                            tmax = tmax, tstepspryr = 12,
-                            search_vol = f.effort["search.vol"], 
-                            fmort.u = f.u, fminx.u = f.minu, fmort.v = f.v, 
-                            fminx.v = f.minv, depth = lme_inputs_grid$depth, 
-                            er = lme_inputs_grid$er, 
-                            pp = lme_inputs_grid$intercept, 
-                            slope = lme_inputs_grid$slope, 
-                            sst = lme_inputs_grid$sst, 
-                            sft = lme_inputs_grid$sbt, use.init = TRUE,
-                            effort = lme_inputs_grid$nom_active_area_m2_relative, 
-                            U.initial = U.initial, V.initial = V.initial, 
-                            W.initial = W.initial, 
-                            Ngrid = nrow(lme_inputs_grid$depth))
-
 
