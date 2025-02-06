8e5c47621181197a6e1d108cc1278845ded3577c
lidefi87 <lidefi87@hotmail.com>
2025-02-06 14:23:28 +1100

    adapting python script to use correcting gridded dbpm model

--- new_workflow/run_dbpm_gridded.py
+++ new_workflow/run_dbpm_gridded.py
@@ -15,11 +15,11 @@ if __name__ == '__main__':
     client = Client(threads_per_worker = 1)
 
     #Name of region and model resolution
-    region = 'fao-88'
-    model_res = '025deg'
+    region = 'fao-58'
+    model_res = '1deg'
     
     #Defining input and output folders
-    base_folder = '/g/data/vf71/la6889/dbpm_inputs/west_antarctica/'
+    base_folder = '/g/data/vf71/la6889/dbpm_inputs/east_antarctica/'
     gridded_folder = os.path.join(base_folder, 'gridded_params', model_res)
     out_folder = os.path.join(base_folder, 'run_fishing', model_res)
     os.makedirs(out_folder, exist_ok = True) 
@@ -29,61 +29,57 @@ if __name__ == '__main__':
                                            model_res, '*obsclim_deptho_*'))[0])['deptho']
     log10_size_bins_mat = xr.open_zarr('outputs/log10_size_bins_matrix.zarr/')['size_bins']
     
-    ## Loading initial predator data
+    ## Loading predator, detritivores and detritus initialisation data
     predators = xr.open_zarr(glob(os.path.join(gridded_folder, 
                                                'predators_*'))[0])['predators'] 
-    
-    #Load predator results for the month before you want to run DBPM
-    # p = xr.open_dataarray(glob(os.path.join(out_folder, 'predators_*_1854-12.nc'))[0])
-    #Subset predator data for the period you want to run the model
-    # predators = predators.sel(time = slice('1849-04', '1849-12'))
-    #Combine both data arrays
-    # predators = xr.concat([p, predators], dim = 'time')
-    
-    # Load detritus and detritivores
-    #Initialisiation files
     detritivores = xr.open_zarr(glob(os.path.join(gridded_folder, 
                                                  'detritivores_*'))[0])['detritivores']
     detritus = xr.open_zarr(glob(os.path.join(gridded_folder, 'detritus_*'))[0])['detritus']
+    
+    # If picking up from a specific time step:
+    # Load predator results for the month before you want to run DBPM
+    #init_time = '1843-08'
+    #predators = xr.open_dataarray(glob(os.path.join(out_folder, f'predators_*_{init_time}.nc'))[0])
+    #detritivores = xr.open_dataarray(glob(os.path.join(out_folder, 
+    #                                                   f'detritivores_*_{init_time}.nc'))[0])
+    #detritus = xr.open_dataarray(glob(os.path.join(out_folder, 
+    #                                               f'detritus_*_{init_time}.nc'))[0])
 
-    #If picking up from a specific time step
-    # detritivores = xr.open_dataarray(glob(os.path.join(out_folder, 
-    #                                                    'detritivores_*_1854-12.nc'))[0])
-    # detritus = xr.open_dataarray(glob(os.path.join(out_folder, 
-    #                                                'detritus_*_1854-12.nc'))[0])
-
+    # Dynamic data
     #Intercept plankton size spectrum
     ui0 = xr.open_zarr(glob(os.path.join(gridded_folder, 'ui0_spinup*'))[0])['ui0']
-
+    
     #Slope plankton size spectrum
     slope = xr.open_zarr(glob(os.path.join(base_folder, 'gridded', 
                                            model_res, '*spinup_slope_*'))[0])['slope']
-    
-    ## Loading effort
-    effort = xr.open_zarr(glob(os.path.join(gridded_folder, 'effort_spinup*'))[0])['effort']
-    
-    # #Load effort for time step DBPM starts
-    # e_start = xr.open_dataarray(glob(os.path.join(out_folder, 'effort_*_1855-01.nc'))[0])
-    # #Subset effort data from the month after DBPM begins
-    # effort = effort.sel(time = slice('1855-02', '1860-01'))
-    # #Combine both data arrays
-    # effort = xr.concat([e_start, effort], dim = 'time')
-    
     #Temperature effects
     pel_tempeffect = xr.open_zarr(glob(
         os.path.join(gridded_folder, 'pel-temp-eff_spinup*'))[0])['pel_temp_eff']
     
     ben_tempeffect = xr.open_zarr(glob(
         os.path.join(gridded_folder, 'ben-temp-eff_spinup*'))[0])['ben_temp_eff']
-    
-    # pel_tempeffect = pel_tempeffect.sel(time = slice('1855-01', '1859-12'))
-    # ben_tempeffect = ben_tempeffect.sel(time = slice('1855-01', '1859-12'))
-    
+
     #Sinking rate
     sinking_rate = xr.open_zarr(
         glob(os.path.join(base_folder, 'gridded', model_res, '*_spinup_er_*'))[0])['export_ratio']
-    
-    # sinking_rate = sinking_rate.sel(time = slice('1855-01', '1859-12'))
+
+    # Loading effort
+    effort = xr.open_zarr(glob(os.path.join(gridded_folder, 'effort_spinup*'))[0])['effort']
+
+    #If running from a specific point in time, then subset data from the month DBPM is meant to 
+    #begin (i.e., one month after predators data)
+    # subset_date = '1843-09'
+    # ui0 = ui0.sel(time = slice(subset_date, None))
+    # slope = slope.sel(time = slice(subset_date, None))
+    # pel_tempeffect = pel_tempeffect.sel(time = slice(subset_date, None))
+    # ben_tempeffect = ben_tempeffect.sel(time = slice(subset_date, None))
+    # sinking_rate = sinking_rate.sel(time = slice(subset_date, None))
+    # #Effort   
+    # #Load effort for time step DBPM starts
+    # e_start = xr.open_dataarray(glob(os.path.join(out_folder, f'effort_*_{subset_date}.nc'))[0])
+    # effort = effort.sel(time = slice('1843-10', None))
+    # #Combine both data arrays
+    # effort = xr.concat([e_start, effort], dim = 'time')
     
     #Running DBPM
     uf.gridded_sizemodel(gridded_folder, predators, detritivores, detritus, pel_tempeffect, 
